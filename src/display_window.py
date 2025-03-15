813eb1b5c80a226b8fb6aee0d45e61ab6e37650a
Tim Mackenzie <theymightbetim@gmail.com>
2025-03-14 23:46:32 -0400

    move examples to example file and import keys from authenticators.txt

--- src/display_window.py
+++ src/display_window.py
@@ -2,21 +2,15 @@ import tkinter as tk
 from tkinter import messagebox, Label
 from authenticator import Authenticator
 
+
+
+
 class AuthenticatorApp:
     def __init__(self, root):
         self.root = root
         root.title("Authenticator")
-        
-        self.authenticators = [
-            {
-                "label": "My Account", 
-                "seed": "JBSW"
-            },
-            {
-                "label": "Test2",
-                "seed": "JB3DPEHP"
-            }
-        ]
+        self.authenticators = []
+        self.get_saved_authenticators()
         for i, authenticator in enumerate(self.authenticators):
             auth = Authenticator(authenticator["label"], authenticator["seed"])
             labels = auth.get_labels()
@@ -46,6 +40,13 @@ class AuthenticatorApp:
             self.seconds_remaining = tk.Label(self.root, text=labels['seconds_remaining'])
             self.seconds_remaining.grid(column=2, row=i, padx=5)
             self.update_timer(self.seconds_remaining, labels['seconds_remaining'])
+    
+    def get_saved_authenticators(self):
+        with open ('authenticators.txt', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                label, seed = line.strip().split(',')
+                self.authenticators.append({"label": label, "seed": seed})
 
 
 if __name__ == "__main__":
